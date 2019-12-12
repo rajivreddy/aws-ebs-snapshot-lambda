ba7680d3cab0ac098a33473c015361678c6d0d80
georgehill19 <16337904+georgehill19@users.noreply.github.com>
2019-12-12 14:41:00 +0000

    Adding slack_notifications script, and adding requests to requirements file

---
+++ ebs_snapshot_lambda/slack_notifications.py
@@ -0,0 +1,92 @@
+#!/usr/bin/env python
+import logging
+
+import requests
+
+logging.basicConfig()
+logger = logging.getLogger("slack_notification")
+logger.setLevel("INFO")
+
+
+class SlackNotificationSetup(object):
+    def __init__(self):
+        self.slack_url = "https://slack-notifications.tax.service.gov.uk/slack-notifications/notification"
+
+    def send_notification(self, slack_url, slack_notifications_password, slack_channel):
+        slack_client = SlackNotification()
+        slack_client.create_ebs_snapshot_lambda_failure_notification(
+            uri=slack_url,
+            slack_notifications_password=slack_notifications_password,
+            slack_channel=slack_channel,
+        )
+
+
+class SlackNotification(object):
+    @staticmethod
+    def post_request_to_slack(
+        uri, payload, slack_notifications_password, slack_channel
+    ):
+        slack_notifications_username = "ebs-snapshot-lambda"
+        slack_notifications_password = slack_notifications_password
+
+        try:
+            response = requests.post(
+                url=uri,
+                json=payload,
+                headers={"Accepts": "application/json"},
+                timeout=10,
+                auth=requests.auth.HTTPBasicAuth(
+                    slack_notifications_username, slack_notifications_password
+                ),
+            )
+            response.raise_for_status()
+            response_body = response.json()
+            if response_body.get("errors") or response_body.get("exclusions"):
+                logger.error("Slack error: {} {}").format(
+                    response_body.get("errors"), response_body.get("exclusions")
+                )
+            if response.status_code == 200:
+                logger.info(
+                    "Slack notification successfully sent to {}".format(slack_channel)
+                )
+            else:
+                logger.error(
+                    "Error when attempting to send slack notification to {}: received {}".format(
+                        slack_channel, response.status_code
+                    )
+                )
+            return response
+        except Exception as e:
+            logger.error("Error: {}".format(e))
+
+    def create_ebs_snapshot_lambda_failure_notification(
+        self, uri, slack_notifications_password, slack_channel, color="#ff0000"
+    ):
+        payload = {
+            "channelLookup": {"by": "slack-channel", "slackChannels": [slack_channel]},
+            "messageDetails": {
+                "text": "Encountered failure when running ebs-snapshot-lambda",
+                "username": "build-and-deploy-apm",
+                "attachments": [
+                    {
+                        "fallback": "Encountered failure when running ebs-snapshot-lambda",
+                        "color": color,
+                        "title": "Encountered failure when running ebs-snapshot-lambda",
+                        "fields": [
+                            {
+                                "title": "Message",
+                                "value": "Check cloudwatch logs for the ebs-snapshot-lambda "
+                                "to investigate cause of failure",
+                                "short": False,
+                            }
+                        ],
+                    }
+                ],
+            },
+        }
+        return self.post_request_to_slack(
+            uri=uri,
+            payload=payload,
+            slack_notifications_password=slack_notifications_password,
+            slack_channel=slack_channel,
+        )
